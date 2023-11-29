fe822cc17260de4d126ad82cd63b876af2bb0136
Usnagiri Srikanth <UsnagiriS@jmf.com>
2023-11-29 14:43:03 +0530

    tab icons

--- src/navigations/BottomTabNav.js
+++ src/navigations/BottomTabNav.js
@@ -2,21 +2,66 @@ import React from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import HomeScreen from '../main/screens/HomeScreen';
 import ProfileScreen from '../main/screens/ProfileScreen';
+import Icon from 'react-native-vector-icons/AntDesign';
 
 const BottomTab = createBottomTabNavigator();
 
 const BottomTabNav = () => {
   return (
-    <BottomTab.Navigator>
+    <BottomTab.Navigator
+      screenOptions={{
+        tabBarLabelStyle: {
+          fontSize: 14,
+          color: '#000',
+          textAlign: 'center',
+        },
+        tabBarStyle: {
+          backgroundColor: '#fff',
+          height: 55,
+        },
+        tabBarActiveTintColor: 'blue',
+      }}>
       <BottomTab.Screen
         name="home"
         component={HomeScreen}
-        options={{headerShown: false}}
+        options={{
+          headerShown: false,
+          tabBarLabel: 'Home',
+          tabBarIcon: ({color, size, focused}) => (
+            <Icon
+              name="home"
+              size={25}
+              color={focused ? 'red' : 'gray'}
+              style={
+                {
+                  // paddingHorizontal: 10,
+                  // marginLeft: -10,
+                }
+              }
+            />
+          ),
+        }}
       />
       <BottomTab.Screen
         name="profile"
         component={ProfileScreen}
-        options={{headerShown: false}}
+        options={{
+          headerShown: false,
+          tabBarLabel: 'Profile',
+          tabBarIcon: ({color, size, focused}) => (
+            <Icon
+              name="user"
+              size={25}
+              color={focused ? 'red' : 'gray'}
+              style={
+                {
+                  // paddingHorizontal: 10,
+                  // marginLeft: -10,
+                }
+              }
+            />
+          ),
+        }}
       />
     </BottomTab.Navigator>
   );
